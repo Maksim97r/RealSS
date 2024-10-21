caff413aac6391a5070e4a309d2bb34b999e08a5
maksim97r <game452@mail.ru>
2024-10-21 21:46:09 +0300

    new Project

---
+++ src/test/java/org/example/realss/RealSsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.realss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealSsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
